e184c0aaf349dc39cc5bbe0569a08a8b62fd381a
Luis Flores <luisfloresc95@gmail.com>
2021-02-16 18:48:08 +0000

    update

--- hooks/dependencyInstaller.js
+++ hooks/dependencyInstaller.js
@@ -2,7 +2,9 @@ console.log("Running hook to install hooks pre-requisites");
 module.exports = function (context) {
   return new Promise((resolve, reject) => {
     var child_process = require('child_process');
-    child_process.exec('npm install', {cwd: __dirname}, function (error) {
+    child_process.exec('npm install', {cwd: __dirname}, function (error, stdout) {
+      console.log('NPM: ' + stdout);
+
       if (error !== null) {
         console.log('exec error: ' + error);
         reject('npm installation failed');
